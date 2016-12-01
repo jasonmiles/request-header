1269c139e0ef3dec9b616d425aea93ea1b0d1a2a
jasonmiles <writejasonmiles@icloud.com>
2016-12-01 18:24:08 +0800

    second commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express()
 var port = process.env.PORT || 8080;
 
 app.get('/api/whoami', function(req, res) {
-  var ipaddress = req.connection.remoteAddress
+  var ipaddress = req.ip
   var lang = req.headers["accept-language"].split(',')[0]
   var software = req.headers["user-agent"].split('(')[1].split(')')[0]
   res.send({ipaddress: ipaddress, language: lang, software: software})
